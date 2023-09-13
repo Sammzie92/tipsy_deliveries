dfd0ba9bb1d8603b7bc04471c807df748caa4958
Sammy Njoroge <missemadranya@gmail.com>
2023-09-13 09:28:12 -0400

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+
+const SetupPage = () => {
+  return (
+    <div>
+        <div className="p-4">        
+        <UserButton afterSignOutUrl="/"/>
+        </div>  
+
+    </div>
+  
+   
+  )
+}
+
+export default SetupPage
